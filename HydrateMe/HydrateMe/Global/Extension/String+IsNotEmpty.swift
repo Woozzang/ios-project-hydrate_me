2387408f831cbf0e7ddf48994425dfa42d1575fb
Woozzang <woozzaang@gmail.com>
2021-10-11 04:12:49 +0900

    feat: Custom Extensions

---
+++ HydrateMe/HydrateMe/Global/Extension/String+IsNotEmpty.swift
@@ -0,0 +1,15 @@
+//
+//  String.swift
+//  HydrateMe
+//
+//  Created by Woochan Park on 2021/10/10.
+//
+
+import Foundation
+
+extension String {
+  
+  var isNotEmpty : Bool {
+    return !self.isEmpty
+  }
+}
